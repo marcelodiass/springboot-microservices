8b30861e9641a7f039993bb6376a6b1306c194cd
Marcelo Dias <marcelodias.desenvolvedor@gmail.com>
2024-10-09 13:41:51 -0300

    inventory service

---
+++ inventory-service/src/main/java/com/marcelodias/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.marcelodias.microservices.inventory.repository;
+
+import com.marcelodias.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
